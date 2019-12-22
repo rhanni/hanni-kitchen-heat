53191f4bfc024584f72168ea106d48cc54bd02a2
Ryan Hanni <ryan.hanni@appfolio.com>
2019-12-22 15:37:49 -0800

    Add bootstrap and get rid of bloat from create-react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Welcome to Kitchen Heat - A blog by Ryan Hanni</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
